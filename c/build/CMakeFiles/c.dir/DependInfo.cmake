40899d2474c95997bc3d8d4cbef64c138b7215a5
Eldar Nugaev <reldan0@gmail.com>
2026-03-20 03:10:08 -0700

    15

---
+++ c/build/CMakeFiles/c.dir/DependInfo.cmake
@@ -0,0 +1,28 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/reldan/myprogs/mylox/c/chunk.c" "CMakeFiles/c.dir/chunk.c.o" "gcc" "CMakeFiles/c.dir/chunk.c.o.d"
+  "/Users/reldan/myprogs/mylox/c/debug.c" "CMakeFiles/c.dir/debug.c.o" "gcc" "CMakeFiles/c.dir/debug.c.o.d"
+  "/Users/reldan/myprogs/mylox/c/main.c" "CMakeFiles/c.dir/main.c.o" "gcc" "CMakeFiles/c.dir/main.c.o.d"
+  "/Users/reldan/myprogs/mylox/c/memory.c" "CMakeFiles/c.dir/memory.c.o" "gcc" "CMakeFiles/c.dir/memory.c.o.d"
+  "/Users/reldan/myprogs/mylox/c/value.c" "CMakeFiles/c.dir/value.c.o" "gcc" "CMakeFiles/c.dir/value.c.o.d"
+  "/Users/reldan/myprogs/mylox/c/vm.c" "CMakeFiles/c.dir/vm.c.o" "gcc" "CMakeFiles/c.dir/vm.c.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ c/build/CMakeFiles/c.dir/cmake_clean.cmake
@@ -0,0 +1,21 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/c.dir/chunk.c.o"
+  "CMakeFiles/c.dir/chunk.c.o.d"
+  "CMakeFiles/c.dir/debug.c.o"
+  "CMakeFiles/c.dir/debug.c.o.d"
+  "CMakeFiles/c.dir/main.c.o"
+  "CMakeFiles/c.dir/main.c.o.d"
+  "CMakeFiles/c.dir/memory.c.o"
+  "CMakeFiles/c.dir/memory.c.o.d"
+  "CMakeFiles/c.dir/value.c.o"
+  "CMakeFiles/c.dir/value.c.o.d"
+  "CMakeFiles/c.dir/vm.c.o"
+  "CMakeFiles/c.dir/vm.c.o.d"
+  "c"
+  "c.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/c.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
